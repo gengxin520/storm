f621c45a6f6828b155b01fed0fefa9664a4fb3a9
Bob Sun <sun@SundeMac-mini.local>
2017-12-22 17:31:57 +0800

    update app

--- src/main/java/com/bob/storm/StormApplication.java
+++ src/main/java/com/bob/storm/StormApplication.java
@@ -1,13 +1,18 @@
 package com.bob.storm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StormApplication {
 
+	protected final static Logger LOGGER = LoggerFactory.getLogger(StormApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(StormApplication.class, args);
+		LOGGER.info("Storm startup success!");
 	}
 
 }
